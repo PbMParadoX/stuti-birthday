883f2b171d680ba355a3ec210235d3391c4d98fc
Priyabrata Moharana <104661225+PbMParadoX@users.noreply.github.com>
2026-01-15 23:23:25 +0530

    Update TARGET_DATE to new birthday time

--- js/config.js
+++ js/config.js
@@ -4,13 +4,10 @@
 const TARGET_DATE = {
   year: 2026,
   month: 0,        // January = 0
-  day: 15,
-  hour: 23,
-  minute: 5,
+  day: 16,
+  hour: 0,
+  minute: 0,
   second: 0
-  // hour: new Date().getHours(),
-  // minute: new Date().getMinutes(),
-  // second: new Date().getSeconds() + 5
 };
 
 // Timezone offset for IST (UTC +5:30)
@@ -21,5 +18,3 @@ const CONFIG = {
   autoRevealIfPassed: true,
   debug: false
 };
-
-
